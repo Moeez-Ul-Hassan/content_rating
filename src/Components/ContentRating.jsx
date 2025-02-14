5a0f395d3531d92e6c9f7b1fdef181dc8165e8d0
Moeez Ul Hassan <moeezjutt11@gmail.com>
2025-02-14 10:34:37 +0500

    Managed Likes and Dislikes

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,50 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
-    
+    this.state={
+        likes:0,
+        dislikes:0,
+        total:0,
+
+        handleLike:()=>{
+          this.setState((prevState)=> ({
+            likes: prevState.likes +1,
+            total: prevState.total+1
+          }));
+
+        },
+        handleDislike:()=>{
+          this.setState((prevState)=> ({
+            dislikes: prevState.dislikes + 1,
+            total: prevState.total+1
+          }));
+        }
+      
+
+    };
+
+
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+        <p>
+          This app manages the likes and Dislikes for you:::
+    
+        </p>
+
+        <div className='rating-buttons'></div>
+        <button className='like-button' onClick={this.state.handleLike}>
+           Like({this.state.likes})
+        </button>
+        <button className='dislike-button' onClick={this.state.handleDislike}>
+           Dislike({this.state.dislikes})
+        </button>
+        <div className='totalrating'>
+          TotalRating: {this.state.total}
+        </div>
+     </div>
      </>
     );
   }
